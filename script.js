d283973ca70649bfa7eedbde50d52c908dd13403
https://github.com/codelorrd <ibrahimtajudeen911@gmail.com>
2022-09-17 01:25:54 +0100

    First Commit on the ASCENSION Project.

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("navigation").onclick = function() {
+    document.getElementById("nav").style.display = "block";
+}
+
+document.getElementById("close-navigation").onclick = function() {
+    document.getElementById("nav").style.display = "none";
+}
